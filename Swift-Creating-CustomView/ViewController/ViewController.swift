c5a84e6ec01efec6505d6d42f4bbb6ff576b70ea
Jeremy Xue <gg831006@gmail.com>
2020-03-28 19:12:25 +0800

    🚧 (ViewController): Add hintTextField to viewController

--- Swift-Creating-CustomView/ViewController/ViewController.swift
+++ Swift-Creating-CustomView/ViewController/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var hintTextField: HintTextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
