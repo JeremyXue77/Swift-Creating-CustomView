e59fdc890d5309777aa9553038c0d2e862e91c90
Jeremy Xue <gg831006@gmail.com>
2020-03-28 15:19:27 +0800

    🎉 (Project): Init git

---
+++ Swift-Creating-CustomView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swift-Creating-CustomView
+//
+//  Created by Jeremy Xue on 2020/3/28.
+//  Copyright © 2020 jeremyxue. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
